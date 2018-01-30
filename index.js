30ba86d09497170b1aca41e1c3b3f782f02414e0
meghansh36 <meghansh.work@gmail.com>
2018-01-31 04:54:52 +0530

    bot2

--- index.js
+++ index.js
@@ -188,8 +188,9 @@ app.get('/scraps', function (req, res) {
             console.log('response ='+ JSON.stringify(response));
             // store.saveGroup(response);
             console.log(response[2].id);
+            for(var i=1;i<response.length();i++){
             flock.callMethod('chat.sendMessage',store.getUserToken(event.userId),{
-                to: response[2].id,
+                to: response[i].id,
                 text: 'I am in trouble. I am unable to send my location for help.'
             },function(err,res){
                 if(!error) {
@@ -199,7 +200,7 @@ app.get('/scraps', function (req, res) {
                     console.log(err)
                 }
             })
-            
+        }
         }
         else{
             console.log('error = '+ error)
