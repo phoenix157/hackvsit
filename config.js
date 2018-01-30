3d6ee4f9ba49382cbe654205c0aa1d48172c0576
Akashdeep Ramnaney <akashdeepramnaney@gmail.com>
2018-01-30 14:09:39 +0530

    chal jaa

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    port: 8080,           // this is the default
+    appId: '8544e52a-d6aa-4cc3-9fa9-6cceed64f8a0',
+    appSecret: '0d7f5d42-3a52-4796-bf74-63385f378aac',
+    endpoint: 'https://pacific-crag-18876.herokuapp.com/'
+};
